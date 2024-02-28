b816ab7bdd632241cb927bf7fa793aff767eddde
Kirill Vladykin <slideow@gmail.com>
2024-02-28 21:22:28 +0300

    remove log message from UpdatesService

--- bot/src/main/java/edu/java/bot/api/updates/UpdatesService.java
+++ bot/src/main/java/edu/java/bot/api/updates/UpdatesService.java
@@ -7,14 +7,11 @@ import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class UpdatesService {
     private final Set<LinkUpdateRequest> updateRequests = new HashSet<>(); // TODO remove when data layer is done
 
     public void createUpdate(LinkUpdateRequest linkUpdateRequest) {
-        log.info("Received update " + linkUpdateRequest);
-
         if (updateRequests.add(linkUpdateRequest)) {
             throw new UpdateAlreadyExistsException("Update с переданным id уже обработан");
         }
