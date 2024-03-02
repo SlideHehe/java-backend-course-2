035fe46f3dfea94806b0a123dc59170c595a6d94
Kirill Vladykin <slideow@gmail.com>
2024-03-02 15:31:54 +0300

    add tests for clients

--- scrapper/src/main/java/edu/java/scrapper/api/links/dto/LinkResponse.java
+++ scrapper/src/main/java/edu/java/scrapper/api/links/dto/LinkResponse.java
@@ -1,7 +1,6 @@
 package edu.java.scrapper.api.links.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 
