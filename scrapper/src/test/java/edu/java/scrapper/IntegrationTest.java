bf12dba9cfbf41617c38f2d0966ad43e7f5033ad
Kirill Vladykin <slideow@gmail.com>
2024-03-13 20:07:35 +0300

    update IntegrationTest

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -56,5 +56,6 @@ public abstract class IntegrationTest {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
+        registry.add("spring.liquibase.enabled", () -> false);
     }
 }
