39916d2ca2afea87bab9501823f21184912105d4
Kirill Vladykin <slideow@gmail.com>
2024-03-14 19:29:48 +0300

    remove UpdateAlreadyExistsException

--- bot/src/main/java/edu/java/bot/api/updates/UpdatesService.java
+++ bot/src/main/java/edu/java/bot/api/updates/UpdatesService.java
@@ -1,6 +1,5 @@
 package edu.java.bot.api.updates;
 
-import edu.java.bot.api.exception.UpdateAlreadyExistsException;
 import edu.java.bot.api.updates.dto.LinkUpdateRequest;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,11 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UpdatesService {
-    private final Set<LinkUpdateRequest> updateRequests = new HashSet<>(); // TODO remove when data layer is done
-
     public void createUpdate(LinkUpdateRequest linkUpdateRequest) {
-        if (!updateRequests.add(linkUpdateRequest)) {
-            throw new UpdateAlreadyExistsException("Update с переданным id уже обработан");
-        }
+
     }
 }
--- bot/src/test/java/edu/java/bot/api/updates/UpdatesServiceTest.java
+++ bot/src/test/java/edu/java/bot/api/updates/UpdatesServiceTest.java
@@ -1,6 +1,5 @@
 package edu.java.bot.api.updates;
 
-import edu.java.bot.api.exception.UpdateAlreadyExistsException;
 import edu.java.bot.api.updates.dto.LinkUpdateRequest;
 import java.net.URI;
 import java.util.List;
@@ -23,24 +22,5 @@ public class UpdatesServiceTest {
             "description",
             List.of(1L, 2L, 3L)
         )));
-    }
-
-    @Test
-    @DisplayName("Создание обновления с уже существующим id")
-    void alreadyExistingUpdate() {
-        // given
-        LinkUpdateRequest linkUpdateRequest = new LinkUpdateRequest(
-            1L,
-            URI.create("https://aboba.com"),
-            "description",
-            List.of(1L, 2L, 3L)
-        );
-        UpdatesService updatesService = new UpdatesService();
-        updatesService.createUpdate(linkUpdateRequest);
-
-        // when-then
-        assertThatThrownBy(() -> updatesService.createUpdate(linkUpdateRequest))
-            .isInstanceOf(UpdateAlreadyExistsException.class)
-            .hasMessage("Update с переданным id уже обработан");
     }
 }
