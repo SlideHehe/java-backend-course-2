104d5d1069957277560dbc053e86c1b0cdc41f88
Kirill Vladykin <slideow@gmail.com>
2024-03-16 20:06:15 +0300

    add JdbcLinkUpdater bean to LinkUpdaterScheduler

--- scrapper/src/main/java/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
@@ -1,13 +1,15 @@
 package edu.java.scrapper.scheduler;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @ConditionalOnProperty(name = "app.scheduler.enable")
 public class LinkUpdaterScheduler {
-    LinkUpdater jdbcLinkUpdater;
+    private final LinkUpdater jdbcLinkUpdater;
 
     @Scheduled(fixedDelayString = "#{@scheduler.interval}")
     public void update() {
