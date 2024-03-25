35e1bce35c6837aa34c08ce8ed49ecda9df2c80a
Kirill Vladykin <slideow@gmail.com>
2024-03-25 19:08:28 +0300

    add criteria template methods findAllBy and findBy, change all positional args to named

--- scrapper/src/main/java/edu/java/scrapper/domain/tgchat/schemabased/jdbc/JdbcTgChatDao.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/tgchat/schemabased/jdbc/JdbcTgChatDao.java
@@ -3,11 +3,13 @@ package edu.java.scrapper.domain.tgchat.schemabased.jdbc;
 import edu.java.scrapper.domain.tgchat.schemabased.TgChat;
 import edu.java.scrapper.domain.tgchat.schemabased.TgChatDao;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
+@SuppressWarnings("MultipleStringLiterals")
 @RequiredArgsConstructor
 @Repository
 public class JdbcTgChatDao implements TgChatDao {
@@ -15,42 +17,52 @@ public class JdbcTgChatDao implements TgChatDao {
 
     @Override
     public List<TgChat> findAll() {
-        return jdbcClient.sql("select chat.id, chat.created_at from chat")
-            .query(TgChat.class)
-            .list();
+        return findAllBy("", Map.of());
     }
 
     @Override
     public List<TgChat> findAllByLinkId(Long linkId) {
-        return jdbcClient.sql(
-                "select chat.id, chat.created_at from chat join chat_link on chat.id = chat_link.chat_id "
-                    + "where chat_link.link_id = ?")
-            .param(linkId)
-            .query(TgChat.class)
-            .list();
+        return findAllBy(
+            "join chat_link on chat.id = chat_link.chat_id where chat_link.link_id = :linkId",
+            Map.of("linkId", linkId)
+        );
     }
 
     @Override
     public Optional<TgChat> findById(Long id) {
-        return jdbcClient.sql("select chat.id, chat.created_at from chat where id = ?")
-            .param(id)
-            .query(TgChat.class)
-            .optional();
+        return findBy(
+            "where id = :id",
+            Map.of("id", id)
+        );
     }
 
     @Override
     public TgChat add(Long id) {
-        return jdbcClient.sql("insert into chat (id) values (?) returning chat.id, chat.created_at")
-            .param(id)
+        return jdbcClient.sql("insert into chat (id) values (:chatId) returning chat.id, chat.created_at")
+            .param("chatId", id)
             .query(TgChat.class)
             .single();
     }
 
     @Override
     public TgChat remove(Long id) {
-        return jdbcClient.sql("delete from chat where id = ? returning chat.id, chat.created_at")
-            .param(id)
+        return jdbcClient.sql("delete from chat where id = :chatId returning chat.id, chat.created_at")
+            .param("chatId", id)
             .query(TgChat.class)
             .single();
+    }
+
+    private List<TgChat> findAllBy(String criteria, Map<String, ?> params) {
+        return jdbcClient.sql("select chat.id, chat.created_at from chat " + criteria)
+            .params(params)
+            .query(TgChat.class)
+            .list();
+    }
+
+    private Optional<TgChat> findBy(String criteria, Map<String, ?> params) {
+        return jdbcClient.sql("select chat.id, chat.created_at from chat " + criteria)
+            .params(params)
+            .query(TgChat.class)
+            .optional();
     }
 }
