bcca9624b72c182832456bf899d6dc2220ea9726
Kirill Vladykin <slideow@gmail.com>
2024-03-28 21:39:55 +0300

    enable caching and rate-limit

--- scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
@@ -4,9 +4,11 @@ import edu.java.scrapper.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableCaching
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
